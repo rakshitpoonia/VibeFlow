5ec98030bc13995c925f067eef4ba42daa1a9bb4
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-01-24 00:49:14 +0530

    removed unnecessary svgs and add profile components in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import UserButton from "@/modules/auth/components/user-button";
 import Image from "next/image";
 
 export default async function Home() {
-  return <Button>Check DB</Button>;
+  return (
+    <>
+      <UserButton />
+    </>
+  );
 }

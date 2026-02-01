1ba98b62a6a9a089a356526995b526bfc91860a6
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-02-01 23:36:09 +0530

    design landing page ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Header } from "@/modules/home/header";
-import Footer from "@/modules/home/footer";
+import { Footer } from "@/modules/home/footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -17,6 +17,15 @@ export default function HomeLayout({
   return (
     <>
       <Header />
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
       <main className="z-20 relative w-full pt-0">{children}</main>
       <Footer />
     </>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ArrowUpRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div className=" z-20 flex flex-col items-center justify-start min-h-screen py-2 mt-10">
+      <div className="flex flex-col justify-center items-center my-5">
+        <Image src={"/hero.svg"} alt="Hero-Section" height={400} width={400} />
 
-const Home = () => {
-  return <div>Home</div>;
-};
+        <h1
+          className=" z-20 text-5xl mt-5 font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-r 
+from-amber-500 via-orange-400 to-orange-300 
+dark:from-amber-400 dark:via-orange-300 dark:to-orange-200
+ "
+        >
+          Vibe Code With Intelligence
+        </h1>
+      </div>
 
-export default Home;
+      <p className="mt-2 text-lg text-center text-gray-600 dark:text-gray-400 px-5 py-10 max-w-2xl">
+        VibeFlow is a high performance web based AI first code editor that
+        enhances your coding experience with advanced features and seamless
+        integration. It is designed to help you write, debug, and optimize your
+        code efficiently.
+      </p>
+      <Link href={"/dashboard"}>
+        <Button variant={"brand"} className="mb-4" size={"lg"}>
+          Get Started
+          <ArrowUpRight className="w-3.5 h-3.5" />
+        </Button>
+      </Link>
+    </div>
+  );
+}

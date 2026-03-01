f9cbdcc96f50aea7321d806f3ac6f1060f24af00
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-03-01 20:04:04 +0530

    congifured next/image to allow all domains for image urls from oauth

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com", // Google avatar urls
-      "avatars.githubusercontent.com", // GitHub profile images
-      "avatars2.githubusercontent.com", // GitHub profile images
-      // …any other external hosts you use
+    // to allow google/github auth profile picture to be displayed without any issues
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };

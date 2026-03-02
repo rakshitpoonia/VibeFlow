ce674c4baf4f865a2c8bab8a0a98b9bf20a72928
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-03-03 00:46:09 +0530

    add nextjs new version starter template

--- lib/template.ts
+++ lib/template.ts
@@ -1,6 +1,6 @@
 export const templatePaths = {
   REACT: "/vibecode-starters/react-ts",
-  NEXTJS: "/vibecode-starters/nextjs",
+  NEXTJS: "/vibecode-starters/nextjs-new",
   EXPRESS: "/vibecode-starters/express-simple",
   VUE: "/vibecode-starters/vue",
   HONO: "/vibecode-starters/hono-nodejs-starter",

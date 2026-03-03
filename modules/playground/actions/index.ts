3193e99b2cf06a607dfaa30bd1cd7ecc53bf1d6b
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-03-03 19:47:36 +0530

    Save Updated code to db server action

--- modules/playground/actions/index.ts
+++ modules/playground/actions/index.ts
@@ -1,6 +1,8 @@
 "user server";
 
 import { db } from "@/lib/db";
+import { TemplateFolder } from "../lib/path-to-json";
+import { currentUser } from "@/modules/auth/actions";
 
 export const getPlaygroundById = async (id: string) => {
   try {
@@ -18,5 +20,33 @@ export const getPlaygroundById = async (id: string) => {
     return playground;
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const SaveUpdatedCode = async (
+  playgroundId: string,
+  data: TemplateFolder,
+) => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  try {
+    const updatedPlayground = await db.templateFile.upsert({
+      where: {
+        playgroundId,
+      },
+      update: {
+        content: JSON.stringify(data),
+      },
+      create: {
+        playgroundId,
+        content: JSON.stringify(data),
+      },
+    });
+
+    return updatedPlayground;
+  } catch (error) {
+    console.log("SaveUpdatedCode error:", error);
+    return null;
   }
 };

61e5af34101a80538d496020fa08edb9c10553fe
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-03-08 01:00:01 +0530

    configure cross origin for webcontainer

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,25 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
